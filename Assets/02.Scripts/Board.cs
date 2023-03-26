f970e4f870065c25d2188f0a93cb48a2c697d13f
ericjangdev_HomeDeskTop <ericjangdev@gmail.com>
2023-03-26 09:35:17 +0900

    DFS System_1

--- Assets/02.Scripts/Board.cs
+++ Assets/02.Scripts/Board.cs
@@ -10,6 +10,7 @@ public class Node_
     public Node_(bool _isWall, int _x, int _z) { isWall = _isWall; x = _x; z = _z; }
 
     public bool isWall;
+    public bool isVisited;
     public Node_ ParentNode;
 
     // G : 시작으로부터 이동했던 거리, H : |가로|+|세로| 장애물 무시하여 목표까지의 거리, F : G + H
@@ -19,15 +20,16 @@ public class Node_
 
 public class Board : MonoBehaviour
 {
-    
-    public Vector3Int bottomLeft, topRight; 
+
+    public Vector3Int bottomLeft, topRight;
     public Vector3Int startPos, targetPos;
     public List<Node_> FinalNodeList;
-    public bool allowDiagonal, dontCrossCorner;
+    public List<Node_> DFSList;
+    public bool dontCrossCorner;
 
     int sizeX, sizeZ;
     Node_[,] NodeArray;
-    Node_ StartNode, TargetNode, CurNode;
+    Node_ StartNode, TargetNode, CurNode, UnreachableNode;
     List<Node_> OpenList, ClosedList;
     Collider[] collision;
     private Transform tr;
@@ -35,7 +37,7 @@ public class Board : MonoBehaviour
     public Vector3 center;
     public Vector3 size;
     public LayerMask layerMask;
-    
+
     // 감지해서 출력
     private void Start()
     {
@@ -43,39 +45,44 @@ public class Board : MonoBehaviour
         // GetWallInfo();
     }
 
-    
+
     public void Test()
     {
-        
-        collision = Physics.OverlapBox(center, size, Quaternion.identity, layerMask);       // 검은 큐브 
+
+        collision = Physics.OverlapBox(center, size, Quaternion.identity, layerMask); // 검은 큐브 
         for (int i = 0; i < collision.Length; i++)
         {
             Debug.Log("collision 물체: " + collision[i].transform.name);
             Debug.Log("collision 좌표: " + collision[i].transform.position);
         }
+
         print(collision.Length);
     }
+
     public void GetWallInfo()
     {
-        sizeX = topRight.x - bottomLeft.x + 1;      // 27
-        sizeZ = topRight.z - bottomLeft.z + 1;      // 27
-        
+        sizeX = topRight.x - bottomLeft.x + 1; // 27
+        sizeZ = topRight.z - bottomLeft.z + 1; // 27
+
         NodeArray = new Node_[sizeX, sizeZ];
-        
+
         for (int i = 0; i < sizeX; i++)
         {
             for (int j = 0; j < sizeZ; j++)
             {
                 bool isWall = false;
                 // collision = Physics.OverlapBox(new Vector3(i + bottomLeft.x, 0f, j + bottomLeft.z), Vector3.one * 0.5f, Quaternion.identity, layerMask);
-                foreach (var collider in Physics.OverlapBox(new Vector3(i + bottomLeft.x, 0f, j + bottomLeft.z), Vector3.one * 0.4f, Quaternion.identity, layerMask))
+                foreach (var collider in Physics.OverlapBox(new Vector3(i + bottomLeft.x, 0f, j + bottomLeft.z),
+                             Vector3.one * 0.4f, Quaternion.identity, layerMask))
                 {
                     if (collider.gameObject.layer == LayerMask.NameToLayer("Wall")) isWall = true;
                     // Debug.Log(collider.gameObject.name);
                 }
+
                 NodeArray[i, j] = new Node_(isWall, i + bottomLeft.x, j + bottomLeft.z);
             }
         }
+
         for (int i = 0; i < sizeX; i++)
         {
             for (int j = 0; j < sizeZ; j++)
@@ -86,35 +93,45 @@ public class Board : MonoBehaviour
         }
 
     }
-    
+
     void OnDrawGizmos()
     {
-        // Gizmos.color = Color.black;
+        Gizmos.color = Color.black;
         // Gizmos.DrawWireCube(center, size * 2);
+
+        if (FinalNodeList.Count != 0)
+            for (int i = 0; i < FinalNodeList.Count - 1; i++)
+                Gizmos.DrawLine(new Vector3(FinalNodeList[i].x, 1f, FinalNodeList[i].z),
+                    new Vector3(FinalNodeList[i + 1].x, 1f, FinalNodeList[i + 1].z));
+        if (DFSList.Count != 0)
+            for (int i = 0; i < DFSList.Count - 1; i++)
+            {
+                Gizmos.DrawCube(new Vector3(DFSList[i].x, 1f, DFSList[i].z), Vector3.one * 0.5f);
+            }
         
-        if(FinalNodeList.Count != 0) for (int i = 0; i < FinalNodeList.Count - 1; i++)
-                Gizmos.DrawLine(new Vector3(FinalNodeList[i].x,1f ,FinalNodeList[i].z), new Vector3(FinalNodeList[i + 1].x,1f, FinalNodeList[i + 1].z));
     }
-    
+
     public void PathFinding()
     {
         // NodeArray의 크기 정해주고, isWall, x, y 대입
-        sizeX = topRight.x - bottomLeft.x + 1;      // 27
-        sizeZ = topRight.z - bottomLeft.z + 1;      // 27
-        
+        sizeX = topRight.x - bottomLeft.x + 1; // 27
+        sizeZ = topRight.z - bottomLeft.z + 1; // 27
+
         NodeArray = new Node_[sizeX, sizeZ];
-        
+
         for (int i = 0; i < sizeX; i++)
         {
             for (int j = 0; j < sizeZ; j++)
             {
                 bool isWall = false;
                 // collision = Physics.OverlapBox(new Vector3(i + bottomLeft.x, 0f, j + bottomLeft.z), Vector3.one * 0.5f, Quaternion.identity, layerMask);
-                foreach (var collider in Physics.OverlapBox(new Vector3(i + bottomLeft.x, 0f, j + bottomLeft.z), Vector3.one * 0.4f, Quaternion.identity, layerMask))
+                foreach (var collider in Physics.OverlapBox(new Vector3(i + bottomLeft.x, 0f, j + bottomLeft.z),
+                             Vector3.one * 0.4f, Quaternion.identity, layerMask))
                 {
                     if (collider.gameObject.layer == LayerMask.NameToLayer("Wall")) isWall = true;
                     // Debug.Log(collider.gameObject.name);
                 }
+
                 NodeArray[i, j] = new Node_(isWall, i + bottomLeft.x, j + bottomLeft.z);
             }
         }
@@ -122,18 +139,20 @@ public class Board : MonoBehaviour
         // 시작과 끝 노드, 열린리스트와 닫힌리스트, 마지막리스트 초기화
         StartNode = NodeArray[startPos.x - bottomLeft.x, startPos.z - bottomLeft.z];
         TargetNode = NodeArray[targetPos.x - bottomLeft.x, targetPos.z - bottomLeft.z];
-
+        UnreachableNode = NodeArray[0, 0]; // 항상 벽인 노드
+        
         OpenList = new List<Node_>() { StartNode };
         ClosedList = new List<Node_>();
         FinalNodeList = new List<Node_>();
+        DFSList = new List<Node_>();
 
-        
         while (OpenList.Count > 0)
         {
             // 열린리스트 중 가장 F가 작고 F가 같다면 H가 작은 걸 현재노드로 하고 열린리스트에서 닫힌리스트로 옮기기
             CurNode = OpenList[0];
             for (int i = 1; i < OpenList.Count; i++)
-                if (OpenList[i].F <= CurNode.F && OpenList[i].H < CurNode.H) CurNode = OpenList[i];
+                if (OpenList[i].F <= CurNode.F && OpenList[i].H < CurNode.H)
+                    CurNode = OpenList[i];
 
             OpenList.Remove(CurNode);
             ClosedList.Add(CurNode);
@@ -148,6 +167,7 @@ public class Board : MonoBehaviour
                     FinalNodeList.Add(TargetCurNode);
                     TargetCurNode = TargetCurNode.ParentNode;
                 }
+
                 FinalNodeList.Add(StartNode);
                 FinalNodeList.Reverse();
 
@@ -155,6 +175,7 @@ public class Board : MonoBehaviour
                 {
                     print(i + "번째는 " + FinalNodeList[i].x + ", " + FinalNodeList[i].z);
                 }
+
                 return;
             }
 
@@ -163,13 +184,48 @@ public class Board : MonoBehaviour
             OpenListAdd(CurNode.x + 1, CurNode.z);
             OpenListAdd(CurNode.x, CurNode.z - 1);
             OpenListAdd(CurNode.x - 1, CurNode.z);
+            
+            // if (CurNode == UnreachableNode)
+            // {
+            //     Node_ TargetCurNode = UnreachableNode;
+            //     while (TargetCurNode != StartNode)
+            //     {
+            //         // FinalNodeList.Add(TargetCurNode);
+            //         TargetCurNode = TargetCurNode.ParentNode;
+            //     }
+// 
+            //     // FinalNodeList.Add(StartNode);
+            //     // FinalNodeList.Reverse();
+            //     return;
+            // }
+                       
+            DFSListAdd(CurNode.x, CurNode.z);
+            DFSListAdd(CurNode.x + 1, CurNode.z);
+            DFSListAdd(CurNode.x, CurNode.z - 1);
+            DFSListAdd(CurNode.x - 1, CurNode.z);
         }
     }
+
+    void DFSListAdd(int checkX, int checkZ)
+    {
+        if (checkX >= bottomLeft.x && checkX < topRight.x + 1 && checkZ >= bottomLeft.z && checkZ < topRight.z + 1 &&
+            !NodeArray[checkX - bottomLeft.x, checkZ - bottomLeft.z].isWall &&
+            !ClosedList.Contains(NodeArray[checkX - bottomLeft.x, checkZ - bottomLeft.z]))
+        {
+            NodeArray[checkX - bottomLeft.x, checkZ - bottomLeft.z].isVisited = true;
+            Node_ NeighborNode = NodeArray[checkX - bottomLeft.x, checkZ - bottomLeft.z];
+            DFSList.Add(NeighborNode);
+        }
+    }
+
+
 
     void OpenListAdd(int checkX, int checkZ)
     {
         // 상하좌우 범위를 벗어나지 않고, 벽이 아니면서, 닫힌리스트에 없다면
-        if (checkX >= bottomLeft.x && checkX < topRight.x + 1 && checkZ >= bottomLeft.z && checkZ < topRight.z + 1 && !NodeArray[checkX - bottomLeft.x, checkZ - bottomLeft.z].isWall && !ClosedList.Contains(NodeArray[checkX - bottomLeft.x, checkZ - bottomLeft.z]))
+        if (checkX >= bottomLeft.x && checkX < topRight.x + 1 && checkZ >= bottomLeft.z && checkZ < topRight.z + 1 &&
+            !NodeArray[checkX - bottomLeft.x, checkZ - bottomLeft.z].isWall &&
+            !ClosedList.Contains(NodeArray[checkX - bottomLeft.x, checkZ - bottomLeft.z]))
         {
             // 코너를 가로질러 가지 않을시, 이동 중에 수직수평 장애물이 있으면 안됨
             if (dontCrossCorner) if (NodeArray[CurNode.x - bottomLeft.x, checkZ - bottomLeft.z].isWall || NodeArray[checkX - bottomLeft.x, CurNode.z - bottomLeft.z].isWall) return;
